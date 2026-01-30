8b293bf7822ed81892263c6c9c83863033cfb850
vadlashubam <vadlashubam47@gmail.com>
2026-01-30 10:44:15 +0000

    ad

--- main.tf
+++ main.tf
@@ -59,9 +59,11 @@ module "eks" {
   enable_cluster_creator_admin_permissions = true
 
   cluster_addons = {
+*/
     aws-ebs-csi-driver = {
       service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
     }
+*/
   }
 
   vpc_id     = module.vpc.vpc_id
@@ -100,7 +102,7 @@ module "eks" {
 data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
-
+/*
 module "irsa-ebs-csi" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.39.0"
@@ -110,4 +112,5 @@ module "irsa-ebs-csi" {
   provider_url                  = module.eks.oidc_provider
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
+} 
+*/
