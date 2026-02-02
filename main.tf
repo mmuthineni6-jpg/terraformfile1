8c35d2f4f66caf2336a81060dbe81ff86814a6a8
vadlashubam <vadlashubam47@gmail.com>
2026-02-02 18:25:51 +0530

    update

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ module "eks" {
     bootstrap_extra_args = "--kubelet-extra-args '--node-labels=type=database'"
   }
 }
-
+}
 # https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/
 data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
