3d0e9207570eaaddfbc71225347c572cecfc2e31
vadlashubam <vadlashubam47@gmail.com>
2026-02-12 20:47:16 +0530

    added kubernetes providers

--- providers.tf
+++ providers.tf
@@ -1,11 +1,9 @@
 data "aws_eks_cluster" "cluster" {
-  name       = module.eks.cluster_name
-  depends_on = [module.eks]  # ensures cluster is created first
+  name = "education-eks"
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name       = module.eks.cluster_name
-  depends_on = [module.eks]
+  name = var.cluster_name
 }
 
 provider "kubernetes" {
