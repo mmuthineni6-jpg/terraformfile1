11ca0aca7cabececc519800eaf48cf5d08ccbbbc
vadlashubam <vadlashubam47@gmail.com>
2026-02-12 20:29:08 +0530

    added kubernetes providers

--- LoadBalancer-controller.tf
+++ LoadBalancer-controller.tf
@@ -4,6 +4,5 @@ module "load_balancer_controller" {
   cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
   cluster_name                     = module.eks.cluster_name       ### check outputs.tf for cluater_name 
-  aws_region                        = var.region
 }
 
