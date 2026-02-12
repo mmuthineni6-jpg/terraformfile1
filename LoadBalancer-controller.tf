93ee6ec5afffa60dea3168d7e7d30495312cb658
vadlashubam <vadlashubam47@gmail.com>
2026-02-12 20:25:31 +0530

    added kubernetes providers

--- LoadBalancer-controller.tf
+++ LoadBalancer-controller.tf
@@ -1,8 +1,9 @@
-module "load_balancer_controller" {
+odule "load_balancer_controller" {
   source = "git::https://github.com/DNXLabs/terraform-aws-eks-lb-controller.git"
 
-  cluster_identity_oidc_issuer     = module.eks_cluster.cluster_oidc_issuer_url
-  cluster_identity_oidc_issuer_arn = module.eks_cluster.oidc_provider_arn
-  cluster_name                     = module.eks.cluster_name
+  cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
+  cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+  cluster_name                     = module.eks.cluster_name       ### check outputs.tf for cluater_name 
+  aws_region                        = var.region
 }
 
