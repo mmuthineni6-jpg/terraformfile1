566027cd25d953212a9045f48731c303f12c242f
vadlashubam <vadlashubam47@gmail.com>
2026-02-17 20:55:36 +0530

    update

--- providers.tf
+++ providers.tf
@@ -1,11 +1,3 @@
-data "aws_eks_cluster" "cluster" {
-  name = "education-eks"
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = "education-eks"
-}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
